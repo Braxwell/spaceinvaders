a114e63390afbb7d53f6686a2d60f4ed83f1ce53
Maxime <gitgud@ds3.com>
2018-05-02 16:38:43 +0200

    2ème commit spaceinvaders

---
+++ src/main/java/fr/unilim/iut/spaceinvaders/Missile.java
@@ -0,0 +1,9 @@
+package fr.unilim.iut.spaceinvaders;
+
+public class Missile {
+
+	public Missile() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -0,0 +1,9 @@
+package fr.unilim.iut.spaceinvaders;
+
+public class Vaisseau {
+
+	public Vaisseau() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
